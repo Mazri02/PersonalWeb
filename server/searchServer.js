9ad94e57e81e40ead3ed617b357a36420dc34a18
Mazri02 <azri.mokhzani@gmail.com>
2023-07-10 21:50:11 +0800

    Change SearchServer

--- server/searchServer.js
+++ server/searchServer.js
@@ -3,56 +3,39 @@ const express = require('express');
 const findAlbum = require('./FindAlbum');
 const mailer = require('nodemailer');
 
-module.exports = (req, res) => {
-  const { method, query } = req;
+module.exports = async (req, res) => {
+  const { query, path } = req;
 
-  switch (method) {
-    case 'GET':
-      if (req.url.startsWith('/SongAlbum')) {
-        handleSongAlbum(query, res);
-      } else if (req.url.startsWith('/sendEmail')) {
-        handleSendEmail(query, res);
-      } else {
-        res.status(404).end();
-      }
-      break;
-    default:
-      res.setHeader('Allow', ['GET']);
-      res.status(405).end(`Method ${method} Not Allowed`);
+  if (path.startsWith('/SongAlbum')) {
+    try {
+      const response = await findAlbum(query.directoryPath);
+      res.status(200).json(response);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else if (path.startsWith('/sendEmail')) {
+    try {
+      const transporter = mailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'azri.mokhzani@gmail.com',
+          pass: 'cnkzstjbhorpdcef'
+        }
+      });
+
+      const mailOptions = {
+        from: query.gmail,
+        to: 'azri.mokhzani@gmail.com',
+        subject: `${query.name} wants to send a message from the website`,
+        html: `${query.text}<br/>${query.phone}`
+      };
+
+      const info = await transporter.sendMail(mailOptions);
+      res.status(200).json({ details: info.response, code: 200 });
+    } catch (error) {
+      res.status(500).json({ detail: error, code: 500 });
+    }
+  } else {
+    res.status(404).end();
   }
 };
-
-function handleSongAlbum(query, res) {
-  findAlbum(query.directoryPath)
-    .then(response => {
-      res.json(response);
-    })
-    .catch(error => {
-      res.status(500).json({ error: error.message });
-    });
-}
-
-function handleSendEmail(query, res) {
-  const transporter = mailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'azri.mokhzani@gmail.com',
-      pass: 'cnkzstjbhorpdcef'
-    }
-  });
-
-  const mailOptions = {
-    from: query.gmail,
-    to: 'azri.mokhzani@gmail.com',
-    subject: `${query.name} wants to send a message from the website`,
-    html: `${query.text}<br/>${query.phone}`
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      res.status(500).json({ detail: error, code: 500 });
-    } else {
-      res.json({ details: info.response, code: 200 });
-    }
-  });
-}
