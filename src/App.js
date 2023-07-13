a953560eee95a2bdc4160663ed4b4d2c57834345
Mazri02 <azri.mokhzani@gmail.com>
2023-07-13 20:04:45 +0800

    Remove Vercel.json

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Homepage from './Component/Project/Project.js';
 import SideBio from './Component/SideBio/SideBio.js';
 import Skills from './Component/Skills/Skills.js';
 import Journey from './Component/Journey/Journey.js';
-import Albums from './Component/Albums/Albums.js';
 import Contact from './Component/Contact/Contact.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -27,10 +26,6 @@ class App extends React.Component{
                 
                 <div className='ContactContainer'>    
                     <Contact/>
-                </div>
-                
-                <div className='AlbumContainer'>
-                    <Albums/>
                 </div>
             </React.Fragment>
         };
