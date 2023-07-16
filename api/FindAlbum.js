5513d80e77d070af08f236ae4ed341307aef8e72
Mazri02 <azri.mokhzani@gmail.com>
2023-07-16 22:30:15 +0800

    Chaneg n fix

--- api/FindAlbum.js
+++ api/FindAlbum.js
@@ -1,18 +1,18 @@
-const fs = require('fs');  
+const fs = require('fs');
 const path = require('path');
 
-function findAlbum(p){
-    return new Promise((resolve,reject) => {
-        const directoryPath = path.join(process.cwd(), 'SongAlbum/'+ p);
-        fs.readdir(directoryPath, function (err, files,directoryPath) {
-            if (err) {
-                reject(err)
-                return console.log('Unable to scan directory: ' + err);
-            } else {
-                resolve(files,directoryPath)
-            }
-        });
-    })  
+function findAlbum(p) {
+  return new Promise((resolve, reject) => {
+    const directoryPath = path.join(process.cwd(), 'SongAlbum/' + p);
+    fs.readdir(directoryPath, function (err, files) {
+      if (err) {
+        reject(err);
+        return console.log('Unable to scan directory: ' + err);
+      } else {
+        resolve(files, directoryPath);
+      }
+    });
+  });
 }
 
 module.exports = findAlbum;
