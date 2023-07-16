f96bbb7f5c05b379fc656eb0289d359856df4c09
Mazri02 <azri.mokhzani@gmail.com>
2023-07-16 22:10:02 +0800

    Change index file

--- api/index.js
+++ api/index.js
@@ -9,7 +9,6 @@ app.use(cors());
 app.get('/SongAlbum/:directoryPath',(req,res) => {
     findAlbum(req.params.directoryPath).then(response => {
         res.json(response)
-        res.json(req.params.directoryPath)
     });
 })
 
